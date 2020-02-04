7a00504f9fdd59261d63d986bbf21497bf55acc5
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-04 19:05:53 -0300

    Mi primer commit
    Mi primer commit

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,12 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	//var nombre;
+	//nombre = prompt ("hola", "nn");
+	//alert(nombre);
+	//alert ("Esto funciona de maravilla")
+	var nombre;
+	nombre = prompt ("Por favor ingrese su nombre","Valeria");
+	alert("nombre");
 }
 
