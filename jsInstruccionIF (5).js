d30c25b56bc3f1c4378bf746966713269356c91b
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-10 20:29:49 -0300

    Hecho
    Utilizamos el || (o ..)

---
+++ jsInstruccionIF (5).js
@@ -0,0 +1,15 @@
+function mostrar()
+{
+	var edad;
+//tomo la edad  
+
+	edad=document.getElementById('edad').value;
+
+
+	if(edad<13||edad>17)
+	{
+		alert("NO es Adolescente");
+	}  
+	
+
+}//FIN DE LA FUNCIÓN
