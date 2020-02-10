31188853eaec25201da56e261f924d81d5afbe1a
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-10 16:50:12 -0300

    Add files via upload
    Rectangulo, Circulo y Materiales.

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,54 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largo;
+	var ancho;
+	var alambre;
 
-}
-function Circulo () 
-{
+
+	largo=document.getElementById('Largo').value;
+	largo=parseInt(largo)
+	ancho=document.getElementById('Ancho').value;
+	ancho=parseInt(ancho);
+
+	alambre=2*(largo+ancho)*3;
+
+	alert("Usted necesita "+alambre+" metros de alambre");
+
 	
 }
+
+function Circulo () 
+{
+	var radio;
+	var alambreradio;
+
+	radio=document.getElementById('Radio').value;
+	radio=parseInt(radio);
+
+	alambreradio=(2*3.14*radio)*3;
+
+	alert("Usted necesita "+alambreradio+" metros de alambre");
+}
+
 function Materiales () 
 {
+	var largo;
+	var ancho;
+	var cemento;
+	var cal;
+
+
+	largo=document.getElementById('Largo').value;
+	largo=parseInt(largo)
+
+	ancho=document.getElementById('Ancho').value;
+	ancho=parseInt(ancho);
+
+	cemento=(largo*ancho)*2
+
+	cal=(largo*ancho)*3
+
+	alert("Usted necesita "+cemento+" bolsas de cemento y " +cal+ " bolsas de cal");
 	
 }
