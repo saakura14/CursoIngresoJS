f7a7e2be909bf1e01b2c4fa5d9ce7315100d3a1d
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-10 20:12:17 -0300

    Hecho

---
+++ jsInstruccionIF (2).js
@@ -0,0 +1,19 @@
+function mostrar()
+{
+	var edad;
+//tomo la edad
+
+	edad=document.getElementById('edad').value;
+
+
+	if(edad>17)
+	{
+		alert("Es mayor");
+	}  
+	else
+	{
+		alert("Es menor");
+	}
+
+
+}//FIN DE LA FUNCIÓN
