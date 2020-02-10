3b8ca18743c6f331984f719329ee40cb8e65c3b6
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-10 20:11:56 -0300

    Hecho

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+//tomo la edad 
+
+    
+    edad=document.getElementById('edad').value;
+
+    if(edad==15)
+    {
+    	alert("niña bonita");
+
+
+    }
+
+
+
 
 
 
