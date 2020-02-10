1f618740552de04de527db12242fc580ffbb4da9
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-10 20:21:27 -0300

    Hecho
    utilizando el &&

---
+++ jsInstruccionIF (4).js
@@ -0,0 +1,18 @@
+function mostrar()
+{
+	var edad;
+//tomo la edad
+
+	edad=document.getElementById('edad').value;
+
+
+	if(edad>12&&edad<18)
+	{
+
+		alert("Adolescente");
+	}  
+	
+
+
+
+}//FIN DE LA FUNCIÓN
