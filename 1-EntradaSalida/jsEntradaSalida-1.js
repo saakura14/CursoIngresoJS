a56d2cfaf6dc17a5ba5f164b44f0d283c447907e
Valeria <valeria@gmail.com>
2020-02-11 19:45:52 -0300

    mi primer commit

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,7 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
+	//git funcionando
 	//var nombre;
 	//nombre = prompt ("hola", "nn");
 	//alert(nombre);
