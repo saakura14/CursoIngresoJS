35182dd5000e9caa5c67f1b0ccdef84afb44a258
Valeria <valeria@gmail.com>
2020-02-11 20:38:11 -0300

    Ejercicio 10 hecho Random

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,28 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	var nota;
+	//Genero el número nota entre 1 y 10 
+	nota=Math.floor((Math.random() * 10) + 1);
+
+	console.log(nota)
+	//Genero el número nota entre 1 y 10 
+
+	if(nota>8)
+	{
+		alert ("EXCELENTE");
+	}else
+	{
+		if(nota<5)
+		{
+			alert("Vamos, la proxima se puede");
+		}else		
+		{
+			alert("APROBO");
+		}
+	}
+
 	
+	
+
 
 }//FIN DE LA FUNCIÓN
