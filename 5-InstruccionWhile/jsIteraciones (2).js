2262c25fe83e38f314723553ad58a13fa813f379
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-18 19:57:41 -0300

    Hecho
    Se pone console.log antes del contador para que vaya en descendente. Y en - 1

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,13 @@
 function mostrar()
-{
-	alert('iteración while');
+{	
+	var contador;
 
+	contador=10;
+	while(contador>0)
+	{ 
+		console.log(contador);
+		contador=contador-1;
+	}
+	alert('Numero ' + contador);
 
 }//FIN DE LA FUNCIÓN
