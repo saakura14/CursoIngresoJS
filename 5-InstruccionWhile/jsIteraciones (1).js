31a1eeab147225d715ffeafaf5a0cf5374484ed8
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-18 19:50:01 -0300

    Hecho
    De acuerdo al lugar donde ponga el console.log es la cantidad correcta que me muestra, siempre ponerlo luego de contador + 1

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,13 @@
 function mostrar()
 {
-	alert('iteración while');
+	contador=0;
+	while(contador <10)
+	{
+		contador=contador+1;
+		console.log(contador);
+
+	}
+	alert('Numero ' + contador);
 
 
 }//FIN DE LA FUNCIÓN
