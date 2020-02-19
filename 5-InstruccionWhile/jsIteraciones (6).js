72c63c9ba530de9d232a6c3e7223df42a810807e
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-18 21:29:29 -0300

    Hecho
    pedir 5 numeros por variable while

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,19 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numeroIngresado;
+
+	while(contador<5)
+	{
+		numeroIngresado=prompt("Ingrese numero");
+		numeroIngresado=parseInt(numeroIngresado);
+		contador=contador+1;
+		acumulador=acumulador+numeroIngresado;
+	}
 
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
