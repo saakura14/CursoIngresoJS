f054227ce5500db15c1cf059350ecc2e2a50379b
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-02-18 21:10:42 -0300

    Hecho
    Uso el  && para que tome todo menos el f o m

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,12 @@
 function mostrar()
 {
+	var sexo = prompt("ingrese f ó m .");
 
-var sexo = prompt("ingrese f ó m .");
-
-
-
-document.getElementById('Sexo').value=sexo;
+	while (sexo!= "f" && sexo="m")
+	{
+		sexo = prompt("Error, ingrese f ó m .");
+	}
+	
+	document.getElementById('Sexo').value=sexo;
 
 }//FIN DE LA FUNCIÓN
