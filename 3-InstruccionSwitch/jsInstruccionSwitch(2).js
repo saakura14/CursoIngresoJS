ae3a7e9e41b0864c9ba2076472037fba84213711
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-03-02 19:15:18 -0300

    Hecho
    Switch case meses del año

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,9 +3,24 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
-
-
-
-
+switch (mesDelAño)
+{
+	case "Julio":
+	case "Agosto":
+		alert ("Abrigate que hace frio.")
+		break;
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+		alert("Ya pasamos el frio, ahora calor!!!.")
+		break;
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+		alert ("Falta para el invierno.")
+}
 }//FIN DE LA FUNCIÓN
