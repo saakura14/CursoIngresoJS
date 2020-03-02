b825e1e3ae5a532ec9d4cd2f08d5f728f6019972
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-03-02 19:28:21 -0300

    Hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,9 +4,19 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
-
-
-
+switch (mesDelAño)
+{
+	case "Febrero":
+		alert ("si tiene 28 días.")
+		break;
+	case "Septiembre":
+	case "Noviembre":
+	case "Abril":
+	case "Junio":
+		alert("si tiene 30 días.")
+		break;
+	default:
+		alert ("si tiene 31 días.")
+		break;
+}
 }//FIN DE LA FUNCIÓN
