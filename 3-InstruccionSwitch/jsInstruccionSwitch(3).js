22ab594d0dd64a56d72e536d11df546608e6a245
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-03-02 19:21:44 -0300

    Hecho
    default:
    case .... :

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -4,6 +4,15 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Febrero":
+		alert ("Este mes no tiene más de 29 días.")
+		break;
+	default:
+		alert ("Este mes tiene 30 o más días")
+		break;
+}
 	
 	
 
