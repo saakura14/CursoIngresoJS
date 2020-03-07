bc250fd30fafa73ade56ae0fdf9a84d580e924e6
saakura14 <60628850+saakura14@users.noreply.github.com>
2020-03-07 13:00:13 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,50 @@
+/*Realizar el algoritmo que permita ingresar los datos de una compra de ingredientes para preparar comida al por mayor , hasta que el cliente quiera:
+peso (entre 10 y 1000)en kilo,
+precio por kilo (más de cero ),
+tipo validad("v";"a";"m")(vegetal,animal o mezcla )
+Si compro más de 100 kilos en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 300 kilos en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo de alimento más caro.
+f) El promedio de precio por kilo en total.*/
+
+
+
 function mostrar()
 {
-  
+	var pesoPorKilo;
+	var precioProducto;
+	var tipoValidad;
+	var respuesta;
+	var contador;
+
+	respuesta="si"; 
+	contador=0;
+
+  	while(respuesta=="si")
+	{
+		do
+		{
+			pesoPorKilo=prompt("Ingrese peso del producto");
+			pesoPorKilo=parseInt(pesoPorKilo);
+		}while(isNaN(pesoPorKilo)promedioEdadMujeres || pesoPorKilo<10 || pesoPorKilo>1000);
+		do
+		{
+			precioProducto=prompt("Ingrese precio del producto");
+			precioProducto=parseInt(precioProducto);
+		}while(isNaN(precioProducto) || precioProducto<0);
+		do
+		{
+			tipoValidad=prompt("Ingrese tipo de validad");
+		}while(!isNaN(tipoValidad) || tipoValidad!="vegetal" && tipoValidad!="animal" && tipoValidad!="mezcla");
+
+		contador++;
+		respuesta=prompt("Desea continuar? si/no");
+	}//todo validado
+
+	acumuladorDeKilos=precio
+
+	//a) El importe total a pagar , bruto sin descuento y...
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,121 @@
+/*Bienvenidos.
+En el ingreso a un viaje en crucero nos solicitan nombre , edad(mayores de 18), sexo("f" o "m") y estado civil("soltero", "casado" o "viudo");
+a) El nombre del hombre casado más joven.
+b) El sexo y nombre del pasajero/a más viejo.
+c) La cantidad de mujeres que hay casadas o viudas.
+d) El promedio de edad entre las mujeres.
+e) El promedio de edad entre los hombres solteros.*/
+
+
 function mostrar()
 {
+	var nombre;
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var respuesta;
+	var contador;
 
+	var contadorHombresSolteros;
+	var acumuladorEdadSoltero;
+	var contadorHombresSolteros;
+	var promedioEdadMujeres;
+	var promedioEdadHombre;
+	var acumuladorEdadMujeres;
+	var acumuladorEdad;
+	var contadorMujeresCasado;
+	var contadorMujeresviudo;
+	var contadorMujeres;
+
+	contador=0;
+	contadorMujeresCasado=0;
+	contadorMujeresviudo=0;
+	contadorMujeres=0
+
+	acumuladorEdad=0;
+	acumuladorEdadMujeres=0;
+	contadorHombresSolteros=0;
+	contadorHombresSolteros=0;
+	acumuladorEdadSoltero=0;
+
+	respuesta="si";
+
+	while(respuesta=="si")
+	{
+		do
+		{
+			nombre=prompt("Ingrese nombre");
+		}while(!isNaN(nombre));
+		do
+		{
+			edad=prompt("Ingreese edad");
+			edad=parseInt(edad);
+		}while(isNaN(edad)|| edad<18 || edad>125);
+		do
+		{
+			sexo=prompt("Ingrese sexo f/m");
+		}while(!isNaN(sexo) || sexo!="f" && sexo!="m");
+		do
+		{
+			estadoCivil=prompt("Ingrese estado civil");
+		}while(!isNaN(estadoCivil) || estadoCivil!="casado" && estadoCivil!="soltero" && estadoCivil!="viudo");
+
+	
+		respuesta=prompt("Desea continuar? si/no");
+
+		//validado
+
+		//a) El nombre del hombre casado más joven.
+		if(contador==0 || sexo=="m" && estadoCivil=="casado" || edad<edadHombreCasadoMasJoven)
+		{
+			edadHombreCasadoMasJoven=edad;
+			edadHombreCasadoMasJovenNombre=nombre;
+		}
+		//b) El sexo y nombre del pasajero/a más viejo.
+		if(contador==0 || edad>edadMaxima)
+		{
+			edadMaxima=edad;
+			edadMaximaSexo=sexo;
+			edadMaximaNombre=nombre;
+		}
+		//c) La cantidad de mujeres que hay casadas o viudas.
+		if(contador==0 || sexo=="f" && estadoCivil=="casado")
+		{
+			contadorMujeresCasado++;
+		}else
+			if(contador==0 || sexo=="f" && estadoCivil=="viudo")
+			{
+				contadorMujeresviudo++;
+			}
+		
+
+		//d) El promedio de edad entre las mujeres.
+		if(contador==0 || sexo=="f")
+		{
+			contadorMujeres++;
+		}
+
+		acumuladorEdadMujeres=acumuladorEdadMujeres+edad;
+
+		//e) El promedio de edad entre los hombres solteros.
+		if(estadoCivil=="soltero" && sexo=="m")
+		{
+			contadorHombresSolteros++;
+		}
+
+		acumuladorEdadSoltero=acumuladorEdadSoltero+edad;
+
+		contador++;
+	}
+
+		promedioEdadHombre=acumuladorEdad/contadorHombresSolteros;
+		promedioEdadMujeres=acumuladorEdad/contadorMujeres;
+
+		document.write("<br> El nombre del hombre casado más joven es " + edadHombreCasadoMasJovenNombre); 
+		document.write("<br> el nombre del pasajero/a más viejo " + edadMaximaNombre + "y el sexo es " + edadMaximaSexo);
+		document.write("<br> La cantidad de mujeres que hay casadas es " + contadorMujeresCasado);
+		document.write("<br> La cantidad de mujeres que hay viudas es " + contadorMujeresviudo);
+		document.write("<br> El promedio de edad entre las mujeres es "+ acumuladorEdadMujeres);
+		document.write("<br> El promedio de edad entre los hombres solteros " + acumuladorEdadSoltero);
 }
+
